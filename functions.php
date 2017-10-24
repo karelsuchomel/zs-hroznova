3eace0bc755c69c861b289069d1ea460edd9484f
Karel Suchomel <karelsuchomel@windowslive.com>
2017-10-25 01:59:39 +0200

    Added frontend styles to TinyMCE editor for better WYSIWYG

--- functions.php
+++ functions.php
@@ -1,10 +1,16 @@
 <?php
+//Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) )
+{
+	exit;
+}
 
 // custom meta boxes for administration
 require_once('custom-meta-box-setup.php');
 
 // import stylesheet
-function zshroznova_resources () {
+function zshroznova_resources () 
+{
 	wp_enqueue_style('style', get_stylesheet_uri());
 
 	wp_register_style('main.css', get_template_directory_uri() . '/assets/css/main.css', false, NULL, 'all' );
@@ -18,20 +24,11 @@ function zshroznova_resources () {
 		"siteURL" => get_site_url()
 	));
 }
-
-// so the previous code actualy runs
 add_action('wp_enqueue_scripts', 'zshroznova_resources');
 
-// Filter wp_nav_menu() to add additional links and other output
-function new_nav_menu_items($items) {
-	$items = $items;
-	return $items;
-}
-add_filter( 'wp_nav_menu_items', 'new_nav_menu_items' );
-
 // theme setup
-function zshroznova_theme_setup(){
-
+function zshroznova_theme_setup()
+{
 	// navigation menus
 	register_nav_menus(array(
 	  'menu-top-bar' => __('Horní lišta'),
@@ -41,124 +38,29 @@ function zshroznova_theme_setup(){
 	// add featured image support
 	add_theme_support('post-thumbnails');
 	// define image sizes
-	set_post_thumbnail_size( 252, 189, true );
+	set_post_thumbnail_size( 300, 225, true );
 	// add post format support
 	add_theme_support('post-formats', array('aside', 'image'));
 
+	// add image size for post headers
+	// https://codex.wordpress.org/Option_Reference#Media
+	update_option( 'thumbnail_size_w', 200 );
+	update_option( 'thumbnail_size_h', 150 );
+	update_option( 'medium_size_w', 400 );
+	update_option( 'medium_size_h', 400 );
+	update_option( 'large_size_w', 720 );
+	update_option( 'large_size_h', 720 );
 }
-
 add_action('after_setup_theme', 'zshroznova_theme_setup');
 
-// Customize Appearance Options
-// Controls (UI in appearance -> customize)
-// Settings (save to Database)
-// Sections (Groups)
-function zsh_theme_customize_register($wp_customize) {
-
-	$wp_customize->add_setting('zsh_base_color', array(
-		'default' => '#27b240',
-		'transport' => 'refresh',
-	));
-
-	$wp_customize->add_setting('zsh_light_base_color', array(
-		'default' => '#39c64e',
-		'transport' => 'refresh',
-	));
-
-	$wp_customize->add_setting('zsh_dark_base_color', array(
-		'default' => '#383838',
-		'transport' => 'refresh',
-	));
-
-	$wp_customize->add_section('zsh_standard_colors', array(
-		'title' => __('Standartní barvy', 'zs-hroznova'), // on screen label
-		'priority' => 30,
-	));
-
-	$wp_customize->add_control( new WP_Customize_Color_Control($wp_customize, 'zsh_base_color_control', array(
-			'label' => __('Základní barva', 'zs-hroznova'),
-			'section' => 'zsh_standard_colors',
-			'settings' => 'zsh_base_color',
-	)) );
-
-	$wp_customize->add_control( new WP_Customize_Color_Control($wp_customize, 'zsh_light_base_color_control', array(
-			'label' => __('Základní barva - světlá', 'zs-hroznova'),
-			'section' => 'zsh_standard_colors',
-			'settings' => 'zsh_light_base_color',
-	)) );
-
-	$wp_customize->add_control( new WP_Customize_Color_Control($wp_customize, 'zsh_dark_base_color_control', array(
-			'label' => __('Základní barva - tmavá', 'zs-hroznova'),
-			'section' => 'zsh_standard_colors',
-			'settings' => 'zsh_dark_base_color',
-	)) );
-
+// use post's front-end styles in TinyMCE text editor
+function zshroznova_theme_add_editor_styles() {
+	add_editor_style( 'assets/css/editor-styles.css' );
 }
+add_action( 'admin_init', 'zshroznova_theme_add_editor_styles' );
 
-add_action('customize_register', 'zsh_theme_customize_register');
+// remove WordPress emojis
+require_once( get_template_directory() . '/inc/remove_wp_emoji.php');
 
-// REMOVE WP EMOJI
-remove_action('wp_head', 'print_emoji_detection_script', 7);
-remove_action('wp_print_styles', 'print_emoji_styles');
-
-remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
-remove_action( 'admin_print_styles', 'print_emoji_styles' );
-
-// Output customize CSS
-function zsh_customize_css() { ?>
-
-	<style>
-		/*zsh_base_color*/
-		nav#top-bar {
-		  background: <?php echo get_theme_mod('zsh_base_color') ?>;
-		}
-		#menu-top-bar #logo-wrapper {
-		  background-color: <?php echo get_theme_mod('zsh_base_color') ?>;
-		}
-		.menu-horni-lista-container ul#menu-horni-lista li a {
-		  background: <?php echo get_theme_mod('zsh_base_color') ?>;
-		}
-		nav#side-list #searchform-container .underline-animated {
-		  background-color: <?php echo get_theme_mod('zsh_base_color') ?>;
-		}
-		nav#side-list ul#menu-bocni-seznam > li:before {
-		  background: <?php echo get_theme_mod('zsh_base_color') ?>;
-		}
-		#agenda-list-wrap .day-segment .date-label .dot:before {
-		  background: <?php echo get_theme_mod('zsh_base_color') ?>;
-		}
-		#search-wrap form.search-again #searchform-container .underline-animated {
-		  background-color: <?php echo get_theme_mod('zsh_base_color') ?>;
-		}
-		#search-wrap .post-wrap h2:hover, #search-wrap .post-wrap h2:active {
-		  color: <?php echo get_theme_mod('zsh_base_color') ?>;
-		}
-		/*zsh_light_base_color*/
-		#menu-top-bar #logo-wrapper:hover {
-		  background-color: <?php echo get_theme_mod('zsh_light_base_color') ?>;
-		}
-		.menu-horni-lista-container ul#menu-horni-lista li a:active {
-		  background: <?php echo get_theme_mod('zsh_light_base_color') ?>;
-		}
-		.icon-hamburger-menu:active {
-		  background: <?php echo get_theme_mod('zsh_light_base_color') ?>;
-		}
-		/*zsh_dark_base_color*/
-		#content-single-page p > a {
-		  color: <?php echo get_theme_mod('zsh_dark_base_color') ?>;
-		}
-		nav#side-list ul#menu-bocni-seznam > li a {
-		  color: <?php echo get_theme_mod('zsh_dark_base_color') ?>;
-		}
-		#posts-wrap .post-wrap .basic-info-wrap > a {
-		  color: <?php echo get_theme_mod('zsh_dark_base_color') ?>;
-		}
-		#search-wrap .post-wrap h2 {
-		  color: <?php echo get_theme_mod('zsh_dark_base_color') ?>;
-		}
-
-	</style>
-
-<?php }
-
-add_action('wp_head', 'zsh_customize_css');
+// WordPress Customize feature fot this theme
+require_once( get_template_directory() . '/inc/theme_customize_features.php');
