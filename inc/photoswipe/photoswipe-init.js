0dcf6a8b731de04ce114eb9fb1d3b0137e5ca3ea
Karel Suchomel <karelsuchomel@windowslive.com>
2018-06-05 07:52:13 +0200

    Photoswipe finally working

--- inc/photoswipe/photoswipe-init.js
+++ inc/photoswipe/photoswipe-init.js
@@ -1,52 +1,185 @@
-// Get anchor tags, that triggers the modal
-var anchorsArray = $("#content-single-page a:has(img)");
+var initPhotoSwipeFromDOM = function(gallerySelector) {
 
-$(anchorsArray).click(function(e)
-{
-	e.preventDefault();
-	//console.log(e);
+	// parse slide data (url, title, size ...) from DOM elements 
+	// (children of gallerySelector)
+	var parseThumbnailElements = function(el) {
+		var numNodes = el.length,
+			items = [],
+			size,
+			item;
 
-	// is it a gallery?
-	var elParent = e.currentTarget.parentElement;
-	if ($(elParent).hasClass('gallery-icon')) {
+		for(var i = 0; i < numNodes; i++) {
 
-		loadLargerImage(e);
-		//drawModalGallery(e);
+			anchorEl = el[i]; // <figure> element
 
-	} else {
+			// include only element nodes 
+			if(anchorEl.nodeType !== 1) {
+				continue;
+			}
 
-		loadLargerImage(e);
+			size = anchorEl.getAttribute('data-size').split('x');
 
+			// create slide object
+			item = {
+				src: anchorEl.getAttribute('href'),
+				w: parseInt(size[0], 10),
+				h: parseInt(size[1], 10)
+			};
+
+			// Wordpress by default does not insert a figcaption under the anchortag
+			// if(anchorEl.children.length > 1) {
+			// 	// <figcaption> content
+			// 	item.title = anchorEl.children[1].innerHTML; 
+			// }
+
+			if(anchorEl.children.length > 0) {
+				// <img> thumbnail element, retrieving thumbnail url
+				item.msrc = anchorEl.children[0].getAttribute('src');
+			} 
+
+			item.el = anchorEl; // save link to element for getThumbBoundsFn
+			items.push(item);
+		}
+
+		return items;
+	};
+
+	// find nearest parent element
+	var closest = function closest(el, fn) {
+		return el && ( fn(el) ? el : closest(el.parentNode, fn) );
+	};
+
+	// triggers when user clicks on thumbnail
+	var onThumbnailsClick = function(e) {
+		e = e || window.event;
+		e.preventDefault ? e.preventDefault() : e.returnValue = false;
+
+		var eTarget = e.target || e.srcElement;
+
+		// find root element of slide
+		var clickedListItem = closest(eTarget, function(el) {
+			return (el.tagName && el.tagName.toUpperCase() === 'A');
+		});
+		console.log(clickedListItem);
+
+		if(!clickedListItem) {
+			return;
+		}
+
+		// find index of clicked item by looping through all child nodes
+		// alternatively, you may define index via data- attribute
+		var clickedGallery = clickedListItem.parentNode,
+			index = clickedListItem.getAttribute('data-pswp-uid') - 1;
+
+		if(index >= 0) {
+			// open PhotoSwipe if valid index found
+			console.log('Open photoswipe on index: ' + index);
+			openPhotoSwipe( index, clickedListItem );
+		}
+		return false;
+	};
+
+	// parse picture index and gallery index from URL (#&pid=1&gid=2)
+	var photoswipeParseHash = function() {
+		var hash = window.location.hash.substring(1),
+		params = {};
+
+		if(hash.length < 5) {
+			return params;
+		}
+
+		var vars = hash.split('&');
+		for (var i = 0; i < vars.length; i++) {
+			if(!vars[i]) {
+				continue;
+			}
+			var pair = vars[i].split('=');  
+			if(pair.length < 2) {
+				continue;
+			}           
+			params[pair[0]] = pair[1];
+		}
+
+		if(params.gid) {
+			params.gid = parseInt(params.gid, 10);
+		}
+
+		return params;
+	};
+
+	var openPhotoSwipe = function(index, galleryElement, disableAnimation, fromURL) {
+		var pswpElement = document.querySelectorAll('.pswp')[0],
+			gallery,
+			options,
+			items;
+
+		items = parseThumbnailElements(galleryElements);
+		console.log(items);
+
+		// define options (if needed)
+		options = {
+
+			// define gallery index (for URL)
+			galleryUID: galleryElement.getAttribute('data-pswp-uid'),
+
+			getThumbBoundsFn: function(index) {
+				// See Options -> getThumbBoundsFn section of documentation for more info
+				var thumbnail = items[index].el.getElementsByTagName('img')[0], // find thumbnail
+					pageYScroll = window.pageYOffset || document.documentElement.scrollTop,
+					rect = thumbnail.getBoundingClientRect(); 
+
+				return {x:rect.left, y:rect.top + pageYScroll, w:rect.width};
+			}
+
+		};
+
+		// PhotoSwipe opened from URL
+		if(fromURL) {
+			if(options.galleryPIDs) {
+				// parse real index when custom PIDs are used 
+				// http://photoswipe.com/documentation/faq.html#custom-pid-in-url
+				for(var j = 0; j < items.length; j++) {
+					if(items[j].pid == index) {
+						options.index = j;
+						break;
+					}
+				}
+			} else {
+				// in URL indexes start from 1
+				options.index = parseInt(index, 10) - 1;
+			}
+		} else {
+			options.index = parseInt(index, 10);
+		}
+
+		// exit if index not found
+		if( isNaN(options.index) ) {
+			return;
+		}
+
+		if(disableAnimation) {
+			options.showAnimationDuration = 0;
+		}
+
+		// Pass data to PhotoSwipe and initialize it
+		gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+		gallery.init();
+	};
+
+	// loop through all gallery elements and bind events
+	var galleryElements = document.querySelectorAll( gallerySelector );
+
+	for(var i = 0, l = galleryElements.length; i < l; i++) {
+		galleryElements[i].setAttribute('data-pswp-uid', i+1);
+		galleryElements[i].onclick = onThumbnailsClick;
 	}
-});
 
-//////////////////////////////////////////////
-
-var pswpElement = document.querySelectorAll('.pswp')[0];
-
-// build items array
-var items = [
-    {
-        src: 'https://placekitten.com/600/400',
-        w: 600,
-        h: 400,
-        msrc: 'path/to/small-image.jpg'
-    },
-    {
-        src: 'https://placekitten.com/1200/900',
-        w: 1200,
-        h: 900,
-        msrc: 'path/to/small-image.jpg'
-    }
-];
-
-// define options (if needed)
-var options = {
-    // optionName: 'option value'
-    // for example:
-    index: 0 // start at first slide
+	// Parse URL and open gallery if it contains #&pid=3&gid=1
+	var hashData = photoswipeParseHash();
+	if(hashData.pid && hashData.gid) {
+		openPhotoSwipe( hashData.pid ,  galleryElements[ hashData.gid - 1 ], true, true );
+	}
 };
 
-// Initializes and opens PhotoSwipe
-var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
-gallery.init();
+// execute above function
+initPhotoSwipeFromDOM('.pswp-gallery');
