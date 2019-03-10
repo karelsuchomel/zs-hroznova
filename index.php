f1af5270b6b27a21beb2a2dc5dff7120806f8808
Karel Suchomel <karelsuchomel@windowslive.com>
2019-03-10 20:23:14 +0100

    REVERTED back to using pagination instead of load MORE button. That will be present in theme v2.

--- index.php
+++ index.php
@@ -29,67 +29,10 @@
 		<?php endwhile; ?>
 
 			</div>
-		<div id="load-more-wrap">
-			<?php echo paginate_links(); ?>
-			<!-- <button id="fetch-older-posts-button">Načíst starší</button> -->
+			<div id="pagination-wrap">
+				<?php echo paginate_links(); ?>
+			</div>
 		</div>
-		</div>
-
-		<script type="text/javascript">
-			el = document.getElementById("fetch-older-posts-button")
-
-			function renderNewPost(postData) {
-				var newPostHTML = "<article id=\"" + postData.id + "\" class=\"post-wrap clear-both\"><a class=\"link-overlay\" href=\"";
-				newPostHTML += postData.link
-				newPostHTML += "\" rel=\"bookmark\"></a>"
-
-				if(postData._links['wp:featuredmedia']) {
-
-					newPostHTML += "<img src=\"" + postData._embedded['wp:featuredmedia'][0].media_details.sizes.thumbnail.source_url + "\" class=\"attachment-post-thumbnail size-post-thumbnail wp-post-image\">"
-				}
-
-				newPostHTML += "<h2>" + postData.title.rendered + "</h2>"
-
-				newPostHTML += "</article>"
-
-				console.log("new post added: " + newPostHTML)
-
-				return newPostHTML
-			}
-
-			function renderNewPageOfPosts(posts) {
-				//console.log(posts);
-
-				let newPageHTML = "<div class=\"single-page-of-posts\">"
-
-				for (var i = 0; i < posts.length; i++) 
-				{
-					newPageHTML += renderNewPost(posts[i])
-				}	
-
-				newPageHTML += "</div>"
-
-				const postsPagesWrapEl = document.getElementById("posts-wrap")
-				postsPagesWrapEl.innerHTML += newPageHTML
-
-				console.log("got to the end!")
-			}
-
-			function fetchOlderPosts()
-			{
-				fetch('http://localhost/zshroznova/wp-json/wp/v2/posts?_embed')
-				.then(function(response) {
-					return response.json()
-				})
-				.then(function(responseJSON) {
-					console.log(responseJSON)
-					renderNewPageOfPosts(responseJSON)
-				});
-
-			}
-
-			el.addEventListener("click", fetchOlderPosts)
-		</script>
 		
 		<?php else : ?>
 		<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
